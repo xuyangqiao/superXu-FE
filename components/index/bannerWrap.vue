05cfcbcb28dfd7d81b284d0affb1ae982412601d
xuyangqiao <283081688@qq.com>
2018-10-21 22:55:32 +0800

    first commit

---
+++ components/index/bannerWrap.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="banner-wrap">
+    123123
+  </div>  
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div v-swiper:mySwiper="swiperOption">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item, index) in bannerList" :key="index">
+          <img :src="item.url">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  components: {},
+
+  async asyncData () {
+    let data = await axios.get('/api/test?id=1&name=xuyangqiao')
+    console.log(data)
+    return {
+      bannerList: data.images
+    }
+  },
+
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 30
+      },
+      bannerList: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
